0bdffb284ccb25b58fea299c95d3565b79a40c37
Bright P.-E. RYMER <brightrymer.developer@outlook.com>
2020-04-24 23:55:31 +0200

    Completed styling the header.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
+
+let mobileMenu = new MobileMenu();
 
 // Autoreload browser rendering (!= browser full page reload) on changes
 if (module.hot) {
