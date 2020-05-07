aa457da040fcea83c3da31f361d8036478fcdeb5
Bright P.-E. RYMER <brightrymer.developer@outlook.com>
2020-05-07 12:22:20 +0200

    Added CORS Solution in order to Test the 'Cloud Secret Function' from localhost.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,22 @@
-exports.handler = function(events, context, callback) {
+exports.handler = function(event, context, callback) {
+    // CORS (Cross-Origin Resource Sharing) Solution (Test Cloud Function from Localhost) - BEGIN
+    // If you'd like to learn more about the topic of CORS I always recommend the Mozilla Developer Network (https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)
+    // The actual solution to this issue is to use Netlify Dev. If you're interested in the Netlify platform it's definitely worth looking into.
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+    }
+    
+    if (event.httpMethod !== "POST") {
+        return callback(null, {
+            statusCode: 200,
+            headers,
+            body: "This was not a POST request"
+        });
+    }
+    // CORS Solution (Test Cloud Function from Localhost) - END
+
+
     const secretContent = `
     <h3>Welcome To The Secret Area</h3>
     <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
@@ -6,8 +24,8 @@ exports.handler = function(events, context, callback) {
 
     let body;
 
-    if (events.body) { // If Text sent by user Exists
-        body = JSON.parse(events.body);
+    if (event.body) { // If Text sent by user Exists
+        body = JSON.parse(event.body);
     } else {
         body = {};
     }
@@ -16,11 +34,13 @@ exports.handler = function(events, context, callback) {
     if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
+            headers,
             body: secretContent
         });
     } else {
         callback(null, {
-            statusCode: 401 // Unauthorized
+            statusCode: 401, // Unauthorized
+            headers
         });
     }
 }
