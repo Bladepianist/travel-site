85e1575a08a731ad9509cf1ae9f5e4b2bcf25baf
Bright P.-E. RYMER <brightrymer.developer@outlook.com>
2020-05-07 11:27:40 +0200

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(events, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    });
+    const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `;
+
+    let body;
+
+    if (event.body) { // If Text sent by user Exists
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+    
+    // Obviously, in the real world, the GitHub would be PRIVATE.
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        });
+    } else {
+        callback(null, {
+            statusCode: 401 // Unauthorized
+        });
+    }
 }
